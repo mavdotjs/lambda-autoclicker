7aebef08590cf0a4ef6299c4178155085948f81d
Chad Freeman <ceo@marinadev.xyz>
2024-08-11 15:29:38 -0400

    updated enigo, cargo is bugging, my cpu is on fire and I have to go clean

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use enigo::{ Enigo, MouseButton };
+use enigo::{ Enigo, MouseButton, MouseControllable };
 
 #[tauri::command]
 fn mouse_button(double: bool, button_id: i32) -> Result<(), String> {
-  let enigo = Enigo::new();
+  let mut enigo = Enigo::new();
   let button: MouseButton = match button_id {
     0 => Ok(MouseButton::Left),
     1 => Ok(MouseButton::Middle),
@@ -18,9 +18,15 @@ fn mouse_button(double: bool, button_id: i32) -> Result<(), String> {
   Ok(())
 }
 
+#[tauri::command]
+fn mouse_pos() -> ( i32, i32 ) {
+  let mut enigo = Enigo::new();
+  return enigo
+}
+
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![mouse_button])
+    .invoke_handler(tauri::generate_handler![mouse_button, mouse_pos])
     .plugin(tauri_plugin_store::Builder::default().build())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
