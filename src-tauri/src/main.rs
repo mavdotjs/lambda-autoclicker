17d7725a003e0081e8e4c1904024db0ac179a86a
Chad Freeman <ceo@marinadev.xyz>
2024-08-11 15:12:18 -0400

    Remove enigo state, not recommended to make another one each time, but that's just what we gotta do rn...

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,27 +3,23 @@
 
 use enigo::{ Enigo, MouseButton };
 
-struct EnigoState(Enigo);
-unsafe impl Send for EnigoState {}
-unsafe impl Sync for EnigoState {}
-
 #[tauri::command]
-fn mouse_button(double: bool, button_id: i32, enigo: tauri::State<EnigoState>) -> Result<(), String> {
+fn mouse_button(double: bool, button_id: i32) -> Result<(), String> {
+  let enigo = Enigo::new();
   let button: MouseButton = match button_id {
     0 => Ok(MouseButton::Left),
     1 => Ok(MouseButton::Middle),
     2 => Ok(MouseButton::Right),
     _ => Err("Invalid button ID")
   }?;
-  enigo.lock_mut().0.mouse_click(button);
-  if double { enigo.lock_mut().0.mouse_click(button) };
+  enigo.mouse_click(button);
+  if double { enigo.mouse_click(button) };
 
   Ok(())
 }
 
 fn main() {
   tauri::Builder::default()
-    .manage(EnigoState(Enigo::new()))
     .invoke_handler(tauri::generate_handler![mouse_button])
     .plugin(tauri_plugin_store::Builder::default().build())
     .run(tauri::generate_context!())
